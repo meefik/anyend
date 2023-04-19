65f014d34de04629f5318f5d47280330df88ef56
mrmoon0986 <92893411+mrmoon0986@users.noreply.github.com>
2023-04-19 11:49:02 +0100

    Create websocket.test.js
    In this test, a WebSocket server is created alongside an HTTP server to listen for incoming connections. Then, two WebSocket clients are created (clientA and clientB) that join different rooms. We test that when clientA sends a message to its room, it receives the message, and clientB does not receive the message since it's in a different room.

---
+++ tests/websocket.test.js
@@ -0,0 +1,55 @@
+import http from 'http';
+import { createWebSocketClient } from './utils';
+import createWebSocketServer from '../path/to/your/websocket/server/module';
+
+describe('WebSocket Server', () => {
+  let server;
+  let wss;
+  const PORT = 3001;
+
+  beforeAll(async () => {
+    server = http.createServer();
+    wss = await createWebSocketServer();
+    server.on('upgrade', (request, socket, head) => {
+      wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
+      });
+    });
+    server.listen(PORT);
+  });
+
+  afterAll(() => {
+    wss.close();
+    server.close();
+  });
+
+  test('Join and leave room, and broadcast message', async (done) => {
+    const clientA = await createWebSocketClient(`ws://localhost:${PORT}`);
+    const clientB = await createWebSocketClient(`ws://localhost:${PORT}`);
+
+    clientA.on('message', (message) => {
+      const data = JSON.parse(message);
+      expect(data).toEqual({ text: 'Hello' });
+      clientA.close();
+      clientB.close();
+      done();
+    });
+
+    clientB.on('message', () => {
+      throw new Error('Client B should not receive the message');
+    });
+
+    clientA.send(JSON.stringify({ action: 'join', room: 'test' }));
+    clientB.send(JSON.stringify({ action: 'join', room: 'other' }));
+
+    setTimeout(() => {
+      clientA.send(
+        JSON.stringify({
+          action: 'send',
+          room: 'test',
+          data: { text: 'Hello' },
+        })
+      );
+    }, 100);
+  });
+});
