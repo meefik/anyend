1d07b191670212901c1d91b06ea9850d407623d9
mrmoon0986 <92893411+mrmoon0986@users.noreply.github.com>
2023-04-19 12:27:16 +0100

    Update websocket.test.mjs

--- tests/websocket.test.mjs
+++ tests/websocket.test.mjs
@@ -1,55 +1,68 @@
+import assert from 'assert';
 import http from 'http';
+import WebSocket from 'ws';
 import { createWebSocketClient } from './utils.mjs';
-import createWebSocketServer from '../lib/api/wss.mjs';
+import createWebSocketServer from '../path/to/your/websocket/server/module.mjs';
 
-describe('WebSocket Server', () => {
+async function testWebSocketServer() {
   let server;
   let wss;
   const PORT = 3001;
 
-  beforeAll(async () => {
-    server = http.createServer();
-    wss = await createWebSocketServer();
-    server.on('upgrade', (request, socket, head) => {
-      wss.handleUpgrade(request, socket, head, (ws) => {
-        wss.emit('connection', ws, request);
-      });
+  server = http.createServer();
+  wss = await createWebSocketServer();
+  server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      wss.emit('connection', ws, request);
     });
-    server.listen(PORT);
   });
+  server.listen(PORT);
 
-  afterAll(() => {
-    wss.close();
-    server.close();
-  });
+  const clientA = await createWebSocketClient(`ws://localhost:${PORT}`);
+  const clientB = await createWebSocketClient(`ws://localhost:${PORT}`);
 
-  test('Join and leave room, and broadcast message', async (done) => {
-    const clientA = await createWebSocketClient(`ws://localhost:${PORT}`);
-    const clientB = await createWebSocketClient(`ws://localhost:${PORT}`);
+  const messagePromise = new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      reject(new Error('Test timed out'));
+    }, 5000);
 
     clientA.on('message', (message) => {
       const data = JSON.parse(message);
-      expect(data).toEqual({ text: 'Hello' });
-      clientA.close();
-      clientB.close();
-      done();
+      assert.deepStrictEqual(data, { text: 'Hello' });
+      clearTimeout(timeout);
+      resolve();
     });
 
     clientB.on('message', () => {
-      throw new Error('Client B should not receive the message');
+      clearTimeout(timeout);
+      reject(new Error('Client B should not receive the message'));
     });
-
-    clientA.send(JSON.stringify({ action: 'join', room: 'test' }));
-    clientB.send(JSON.stringify({ action: 'join', room: 'other' }));
-
-    setTimeout(() => {
-      clientA.send(
-        JSON.stringify({
-          action: 'send',
-          room: 'test',
-          data: { text: 'Hello' },
-        })
-      );
-    }, 100);
   });
-});
+
+  clientA.send(JSON.stringify({ action: 'join', room: 'test' }));
+  clientB.send(JSON.stringify({ action: 'join', room: 'other' }));
+
+  setTimeout(() => {
+    clientA.send(
+      JSON.stringify({
+        action: 'send',
+        room: 'test',
+        data: { text: 'Hello' },
+      })
+    );
+  }, 100);
+
+  await messagePromise;
+
+  wss.close();
+  server.close();
+}
+
+(async () => {
+  try {
+    await testWebSocketServer();
+    console.log('WebSocket Server Test: PASSED');
+  } catch (error) {
+    console.error('WebSocket Server Test: FAILED', error);
+  }
+})();
